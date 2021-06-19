346a20be2a922b6c3e7d446b02936da2e09a65de
Ramesh314 <84651490+Ramesh314@users.noreply.github.com>
2021-06-19 09:47:21 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,15 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+
+
+
+float average(int [], int);
+
+int main(void) {
+  int x[] = {9,5,6,10,2,-3,4};
+  assert ((average(x, 7) - 4.7142) < 0.001);
+
+  int y[] = {5};
+  assert (average(y, 1) == 5.0);
+  printf("Average: passed\n");
+  }
